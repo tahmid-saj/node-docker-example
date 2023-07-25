656f030a5a01a7d0628703c225aae3def7d81fff
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-24 20:12:56 -0400

    Adding docker volumes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const db = knex({
 });
 
 const app = express();
-
+console.log("check")
 app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
